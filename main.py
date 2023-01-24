17c071edd6f8015efaee9e4f6553db3ef01bfc55
alex-udodik <alex.udodik@wsu.edu>
2023-01-24 00:25:05 -0800

    Added filtering and sorting for names for birthdays next month

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+
+from datetime import datetime
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+def getNextMonth():
+    currentMonth = datetime.now().month
+
+    if currentMonth == 12:
+        currentMonth = 1
+    else:
+        currentMonth += 1
+
+    return currentMonth
+
+
+load_dotenv()
+
+MONGO_USER = os.getenv('MONGO_USER')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+MONGO_URL = os.getenv('MONGO_URL')
+
+client = MongoClient("mongodb+srv://"+MONGO_USER+":" + MONGO_PASSWORD + "@" + MONGO_URL + "/?retryWrites=true&w=majority")
+db = client.FUPC
+book = db.Members
+
+members = book.find()
+
+targetMonth = getNextMonth()
+
+birthdaysForTargetMonth = []
+
+for x in members:
+    month = x["Birthdate"].month
+    if month == targetMonth:
+        birthdaysForTargetMonth.append(x)
+
+sortedNamesByDay = sorted(birthdaysForTargetMonth, key=lambda i: i["Birthdate"].day)
+
+for x in sortedNamesByDay:
+    print(x)
